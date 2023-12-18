37eaa4f5769560559481973d18ba3be647089622
Colin Matteson <seefm@proton.me>
2023-12-18 17:29:19 -0500

    added UpperHalfPlayer class, moving onto final class and tests

--- UpperHalfPlayer.cs
+++ UpperHalfPlayer.cs
@@ -2,7 +2,12 @@ namespace ShootingDice;
 // TODO: Complete this class
 
 // A Player whose role will always be in the upper half of their possible rolls
-public class UpperHalfPlayer
+public class UpperHalfPlayer : Player
 {
-
+        public override int Roll()
+       { 
+        // Return a random number between 1 and DiceSize
+        int ranNumHigh = new Random().Next(3,6) + 1;
+        return ranNumHigh;
+        }
 }
